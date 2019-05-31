833bf59e6c31e6b263ca2419f6d62e0f2b70729d
Guto-Alves <gustavo.almeida13@fatec.sp.gov.br>
2019-05-30 23:49:14 -0300

    Add p2

--- src/p2/Livro.java
+++ src/p2/Livro.java
@@ -1,5 +1,23 @@
 package p2;
 
 public class Livro {
-
+	int cod;
+	int categoria;
+	String nomeDaCategoria;
+	int qtdEmprestado;
+	
+	public Livro() {
+		this(0, 0, "", 0);
+	}
+	
+	public Livro(int cod, int categoria, String nomeDaCategoria, int qtdEmprestado) {
+		this.cod = cod;
+		this.categoria = categoria;
+		this.nomeDaCategoria = nomeDaCategoria;
+		this.qtdEmprestado = qtdEmprestado;
+	}
+	
+	public String toString() {
+		return String.format("%d %d %s %d%n", cod, categoria, nomeDaCategoria, qtdEmprestado);
+	}
 }
--- src/p2/Main.java
+++ src/p2/Main.java
@@ -1,5 +1,98 @@
 package p2;
 
-public class Main {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Livro[] livros = new Livro[4];
+		
+		int option;
+		do {
+			option = Integer.parseInt(JOptionPane.showInputDialog("1 - Cadastrar livros\n2 - Mostrar livro(s) mais emprestado(s)\n3 - Mostrar quantidade por categoria\n9 - Fim"));
+			
+			switch(option) {
+			case 1:
+				livros = cadastrar(livros);
+				break;
+			case 2:
+				mostrarMaisEmprestado(livros);
+				break;
+			case 3:
+				mostrarPorCategoria(livros);
+				break;
+			case 9:
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "Opcao inválida!");
+				break;
+			}
+		} while(option != 9);
+	}
+	
+	public static Livro[] cadastrar(Livro[] livros) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter("livros.txt"));
+		
+		for(int i = 0; i < livros.length; i++) {
+			livros[i] = new Livro();
+			livros[i].cod = i;
+			livros[i].categoria = Integer.parseInt(JOptionPane.showInputDialog("Categoria: "));
+			livros[i].nomeDaCategoria = JOptionPane.showInputDialog(null, "Nome da categoria: ");
+			livros[i].qtdEmprestado = Integer.parseInt(JOptionPane.showInputDialog("Quantidade de vezes que foi emprestado: "));
+			
+			writer.write(""+livros[i]);
+		}
+		writer.close();
+		
+		return livros;
+	}
+	
+	public static void mostrarMaisEmprestado(Livro[] livros) {
+		int qtdDoMaisEmprestado = livros[0].qtdEmprestado;
+		
+		for(int i = 1; i < livros.length;  i++) {
+			if(livros[i].qtdEmprestado > qtdDoMaisEmprestado)
+				qtdDoMaisEmprestado = livros[i].qtdEmprestado;
+		}
+		String result = "Código do(s) livro(s) mais emprestado(s):\n";
+		
+		for(int i = 0; i < livros.length; i++) {
+			if(livros[i].qtdEmprestado == qtdDoMaisEmprestado) 
+				result += " - " + livros[i].cod + " - ";
+		}
+		
+		JOptionPane.showMessageDialog(null, result);
+	}
+
+	public static void mostrarPorCategoria(Livro[] livros) {
+		for(int i = 0; i < livros.length - 1; i++) {
+			for(int j = i + 1; i < livros.length; i++) {
+				if(livros[i].categoria > livros[j].categoria) {
+					Livro livroAux = livros[i];
+					livros[i] = livros[j];
+					livros[j] = livroAux;
+				}
+			}
+		}
+		String result ="";
+		int categoriaAtual = livros[0].categoria;
+		String nomeCategoria = livros[0].nomeDaCategoria;
+		int qtdCategoria = 1;
+		
+		for(int i = 1; i < livros.length; i++) {
+			if(livros[i].categoria == categoriaAtual)
+				qtdCategoria++;
+			else {
+				result += String.format("Categoria - %s: %d%n", nomeCategoria, qtdCategoria);
+				categoriaAtual = livros[i].categoria;
+				nomeCategoria = livros[i].nomeDaCategoria;
+				qtdCategoria = 1;
+			}
+		}
+		result += String.format("Categoria - %s: %d%n", nomeCategoria, qtdCategoria);
+		
+		JOptionPane.showMessageDialog(null, result);
+	}
 }
