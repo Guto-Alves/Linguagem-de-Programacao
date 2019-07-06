eae40c1c5d0cfb4ba7c8c0a52e22de15aa3e5d21
Gustavo Alves <gustavo.almeida13@fatec.sp.gov.br>
2019-07-05 22:20:56 -0300

    Update Lt01_VetorEx02.java

--- src/vetor/Lt01_VetorEx02.java
+++ src/vetor/Lt01_VetorEx02.java
@@ -1,7 +1,7 @@
 /*
  * Objetivo: Criar e coletar um vetor [100] inteiro e exibir:
  * a. O maior e o menor valor;
- * b. A média dos valores.
+ * b. A mÃ©dia dos valores.
  * 
  * Autor: Gustavo Alves
  * Data: 22/03/2019
@@ -14,25 +14,25 @@ import java.util.Scanner;
 public class Lt01_VetorEx02 {
 
 	public static void main(String[] args) {
-		int[] array = new int[3];
+		int[] array = new int[100];
 
-		array = fillArray(array);
-		maiorEMenor(array);
-		System.out.printf("Média dos valores é de %,.1f", averageArray(array));
+		array = fill(array);
+		displaySmallestAndLargest(array);
+		System.out.printf("MÃ©dia dos valores Ã© de %,.1f", average(array));
 	}
 
-	public static int[] fillArray(int[] array) {
+	public static int[] fill(int[] array) {
 		Scanner scanner = new Scanner(System.in);
 
 		for (int i = 0; i < array.length; i++) {
-			System.out.printf("%d° Valor: ", i + 1);
+			System.out.printf("%dÂ° Valor: ", i + 1);
 			array[i] = scanner.nextInt();
 		}
 		scanner.close();
 		return array;
 	}
 
-	public static void maiorEMenor(int[] array) {
+	public static void displaySmallestAndLargest(int[] array) {
 		int smaller = array[0];
 		int largest = array[0];
 
@@ -43,11 +43,11 @@ public class Lt01_VetorEx02 {
 				smaller = array[i];
 		}
 
-		System.out.printf("O maior valor é %d%n", largest);
-		System.out.printf("O menor valor é %d%n", smaller);
+		System.out.printf("O maior valor Ã© %d%n", largest);
+		System.out.printf("O menor valor Ã© %d%n", smaller);
 	}
 
-	public static double averageArray(int[] array) {
+	public static double average(int[] array) {
 		double average = 0;
 
 		for (int valor : array)
