51a81ae156d4917d32b32f8f09671471c23cd008
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2019-10-08 11:37:39 -0300

    Fix RecursividadeEx1.java

--- src/recursao/RecursividadeEx1.java
+++ src/recursao/RecursividadeEx1.java
@@ -12,12 +12,12 @@ import javax.swing.JOptionPane;
 public class RecursividadeEx1 {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Resultado da série = %d", "Serie = (1+2+3+...+100)",
-				JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, String.format("Resultado da série = %d", calculaSerie(1)),
+				"Serie = (1+2+3+...+100)", JOptionPane.INFORMATION_MESSAGE);
 	}
 
-	public static int calculaSerie1(int number) {
-		return number == 1 ? 1 : number + calculaSerie1(number - 1);
+	public static int calculaSerie(int number) {
+		return number == 100 ? 100 : number + calculaSerie(number + 1);
 	}
 
 }
