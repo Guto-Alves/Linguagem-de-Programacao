be4d81f5a0ddbc6f57619cae73035794a013c19f
Gustavo Alves <gustavo.almeida13@fatec.sp.gov.br>
2020-03-02 17:38:05 -0300

    Refactor exercise Lt01_Proc16.java

--- src/procedimento/Lt01_Proc16.java
+++ src/procedimento/Lt01_Proc16.java
@@ -1,12 +1,12 @@
 /*
  * Objetivo     : 16. Receba a quantidade de horas trabalhadas, o valor por hora, o percentual
- * de desconto e o n˙mero de descendentes. Calcule o sal·rio que ser„o as
- * horas trabalhadas x o valor por hora. Calcule o sal·rio lÌquido (= Sal·rio
- * Bruto ñ desconto). A cada dependente ser· acrescido R$ 100 no Sal·rio
- * LÌquido. Exiba o sal·rio a receber.
+ * de desconto e o n√∫mero de descendentes. Calcule o sal√°rio que ser√£o as
+ * horas trabalhadas x o valor por hora. Calcule o sal√°rio l√≠quido (= Sal√°rio
+ * Bruto ‚Äì desconto). A cada dependente ser√° acrescido R$ 100 no Sal√°rio
+ * L√≠quido. Exiba o sal√°rio a receber.
  * 
  * Programador  : Gustavo Alves
- * Data CriaÁ„o : 7 de mar de 2019
+ * Data Cria√ß√£o : 7 de mar de 2019
  */
 
 package procedimento;
@@ -20,17 +20,17 @@ public class Lt01_Proc16 {
 				.parseDouble(JOptionPane.showInputDialog("Digite a quantidade de horas trabalhadas"));
 		double valorHora = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor por hora"));
 		double percentualDesconto = Double.parseDouble(JOptionPane.showInputDialog("Digite o percentual de desconto"));
-		double numeroDescendentes = Double.parseDouble(JOptionPane.showInputDialog("Digite o n˙mero de descendentes"));
+		double numeroDescendentes = Double.parseDouble(JOptionPane.showInputDialog("Digite o n√∫mero de descendentes"));
 
 		calculaSalario(horasTrabalhadas, valorHora, percentualDesconto, numeroDescendentes);
 	}
 
-	public static void calculaSalario(double horasTrabalhadas, double valorHora, double percentualDesconto,
+	public static void calcularSalario(double horasTrabalhadas, double valorHora, double percentualDesconto,
 			double numeroDescendentes) {
 
 		double salario = ((horasTrabalhadas * valorHora) - percentualDesconto) + 100 * numeroDescendentes;
 
-		JOptionPane.showMessageDialog(null, String.format("Sal·rio a receber R$ %,.2f", salario), "Sal·rio",
+		JOptionPane.showMessageDialog(null, String.format("Sal√°rio a receber R$ %,.2f", salario), "Sal√°rio",
 				JOptionPane.INFORMATION_MESSAGE);
 
 	}
